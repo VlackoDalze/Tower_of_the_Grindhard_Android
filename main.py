2e953c41a590f5522531d1edf18bd02fe675bf63
VlackoDalze <bladimil124@gmail.com>
2023-05-01 13:06:31 +0200

    hellow

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
 
 # class Player(Widget):
 #     pass
@@ -11,11 +15,12 @@ from kivy.uix.button import Button
 # class GameMap(Widget):
 #     pass
 
-class MainWidget(Widget):
+class Control(Widget):
     pass
 
 class TowerOfTheGrindhardApp(App):
-    pass
+    def build(self):
+        return BoxLayout()
 
 if __name__ == '__main__':
     TowerOfTheGrindhardApp().run()
