b8742413bcd8667c066a53491eca15ff7473bb62
GaD-test <faqlmao@gmail.com>
2023-05-01 18:18:52 +0200

    avance

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.vector import Vector
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.lang import Builder
+# organiza los widgets de manera relativa, es decir, se ajustan automáticamente a la posición de los widgets circundantes
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.app import App
 import kivy
 kivy.require('2.1.0')
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout  # organiza los widgets de manera relativa, es decir, se ajustan automáticamente a la posición de los widgets circundantes
-from kivy.lang import Builder
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
-from kivy.core.window import Window
 
 # class Player(Widget):
 #     pass
@@ -22,23 +23,18 @@ from kivy.core.window import Window
 class Root(BoxLayout):
     pass
 
+
 class Control(Widget):
     pass
 
-class Map_lvl(FloatLayout):
-    lvl = NumericProperty(0) #definimos la propiedad lvl se puede acceder a ella desde ambos lados self. root.
-    def next_lvl(self):
-        self.lvl+=1
-    def saved_lvl(self, value):
-        self.lvl=value
-        
+
 class TowerOfTheGrindhardApp(App):
     def build(self):
         return Root()
-    
+
     # def on_start(self): #maximizar
     #     Window.maximize()
 
+
 if __name__ == '__main__':
     TowerOfTheGrindhardApp().run()
-    
