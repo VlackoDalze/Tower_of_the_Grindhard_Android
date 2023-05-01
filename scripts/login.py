46920d047f737c66c52c3bdb37002af98ca7e394
GaD-test <faqlmao@gmail.com>
2023-05-01 22:37:30 +0200

    matriz passwords

--- scripts/login.py
+++ scripts/login.py
@@ -1,6 +1,6 @@
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import NumericProperty
-
+aux_BBDD=[{'admin': 'admin'},{'user01': 'gamba'}]
 
 class Login(RelativeLayout):
 
