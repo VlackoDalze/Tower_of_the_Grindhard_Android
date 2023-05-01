520414b8cf8fb8b1ff55861071e4d20c13a3fdfa
GaD-test <faqlmao@gmail.com>
2023-05-01 16:54:40 +0200

    mapa principal
    mapa cargado, se implemento next lvl, y set lvl

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout  # organiza los widgets de manera relativa, es decir, se ajustan automáticamente a la posición de los widgets circundantes
+from kivy.lang import Builder
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
-
-# class Player(Widget):
-#     pass
-
-
-# class GameMap(Widget):
-#     pass
+from kivy.core.window import Window
 
 class Control(Widget):
     pass
 
+class Map_lvl(FloatLayout):
+    lvl = NumericProperty(0) #definimos la propiedad lvl se puede acceder a ella desde ambos lados self. root.
+    def next_lvl(self):
+        self.lvl+=1
+    def saved_lvl(self, value):
+        self.lvl=value
+        
 class TowerOfTheGrindhardApp(App):
     def build(self):
-        return BoxLayout()
-
+        # return BoxLayout()
+        return Map_lvl()
+    
+    # def on_start(self): #maximizar
+    #     Window.maximize()
+        
+        
 if __name__ == '__main__':
     TowerOfTheGrindhardApp().run()
+    
