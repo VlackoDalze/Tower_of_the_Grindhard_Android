9a76484f16a791a92e03fac383dd059ae0f0b270
VlackoDalze <bladimil124@gmail.com>
2023-05-02 03:35:05 +0200

    solve

--- scripts/control.py
+++ scripts/control.py
@@ -1,11 +1,27 @@
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.properties import BooleanProperty
 
 
 class Control(FloatLayout):
-    pass
+    def openMap(self):
+        print("map")
 
+    def openInventory(self):
+        print("Inventory")
+
+    def movePlayer(self,movement:str):
+        print(movement)
 
 class SettingButton(Button):
-    def movePlayer(self, movement):
-        print(movement)
+    def openSettings(self):
+        print("asd")
+
+class Settings(FloatLayout):
+    visible = BooleanProperty(False)
+
+    def show_layout(self):
+        self.visible = True
+
+    def hide_layout(self):
+        self.visible = False
